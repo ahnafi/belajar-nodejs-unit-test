b9b92434e8dd96af05feac02b3115627bee05328
ahnafi <devsulthon@gmail.com>
2023-12-24 17:14:11 +0700

    menambah babel babel-jest dan config

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,7 +1,7 @@
-import { sum } from "src/sum.js";
+import { sum } from "../src/sum.js";
 
-test("sum(1,2) must be 3", () => {
-  const result = sum(1, 2);
+test("test ajah harusnya keluar", () => {
+  const harusnya = sum(2, 3);
 
-  expect(result).toBe(3);
+  expect(harusnya).toBe(5);
 });
