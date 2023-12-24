36e44f363dd4626b065dc37a2e0c4918782f9545
ahnafi <devsulthon@gmail.com>
2023-12-24 21:32:00 +0700

    jest configuration and try jest command line interface

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,7 +1,16 @@
 import { sum } from "../src/sum.js";
 
-test("test ajah harusnya keluar", () => {
+test("test sum 1 , sum(2,3) expect 5", () => {
   const harusnya = sum(2, 3);
-
   expect(harusnya).toBe(5);
+});
+
+test("test sum 2 , sum(1,3) expect 4", () => {
+  const harusnya = sum(1, 3);
+  expect(harusnya).toBe(4);
+});
+
+test("test sum 3 , sum(6,3) expect 9", () => {
+  const harusnya = sum(6, 3);
+  expect(harusnya).toBe(9);
 });
