4d70b769a7c8afad026e4a4e276c6eb6f9ca6bb8
ahnafi <devsulthon@gmail.com>
2023-12-24 22:56:26 +0700

    menambah numbers mathcer

---
+++ test/numbers.test.js
@@ -0,0 +1,16 @@
+test('numbers', () => {
+    let numbers = 2+2;
+
+    expect(numbers).toBeGreaterThan(3)
+    expect(numbers).toBeGreaterThanOrEqual(3)
+    expect(numbers).toBeLessThan(5)
+    expect(numbers).toBeLessThanOrEqual(4)
+});
+
+test('adding positive numbers is not zero', () => {
+    for (let a = 1; a < 10; a++) {
+      for (let b = 1; b < 10; b++) {
+        expect(a + b).not.toBe(0);
+      }
+    }
+  });
